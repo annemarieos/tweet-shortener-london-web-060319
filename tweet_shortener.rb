999b48435266bc62cd1d3111887011661c832d8d
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-05-05 11:24:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,7 +15,7 @@ end
 def word_substituter(string)
   string.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
-      word = dictionary[word]
+      word = dictionary[word.downcase]
     else
       word
     end
